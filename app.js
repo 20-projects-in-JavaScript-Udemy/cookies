210cfa6308de42eef863f9d92b2aa91985953aa6
roy <roy.framery@gmail.com>
2022-10-03 17:10:04 +0200

    implement HTML, CSS and a part of Javascript handle creation of cookies

---
+++ app.js
@@ -0,0 +1,72 @@
+const inputs = document.querySelectorAll("input")
+
+// inputs.forEach(input => {
+//   input.addEventListener("invalid", handleValidation)
+//   input.addEventListener("input", handleValidation)
+// })
+
+// function handleValidation(e){
+//   if(e.type === "invalid") {
+//     e.target.setCustomValidity("Ce champ ne peut être vide.")
+//   }
+//   else if (e.type === "input"){
+//     e.target.setCustomValidity("")
+//   }
+// }
+
+const cookieForm = document.querySelector("form")
+cookieForm.addEventListener("submit", handleForm)
+
+function handleForm(e){
+  e.preventDefault()
+
+  const newCookie = {};
+
+  inputs.forEach(input => {
+    const nameAttribute = input.getAttribute("name")
+    newCookie[nameAttribute] = input.value;
+  })
+  newCookie.expires = new Date(new Date().getTime() + 7 * 24 * 60 * 60 * 1000)
+  cookieForm.reset()
+
+  createCookie(newCookie)
+}
+
+function createCookie(newCookie){
+
+  if(doesCookieExist(newCookie.name)) {
+    createToast({name: newCookie.name, state: "modifié", color: "orangered"})
+  } 
+  else {
+    createToast({name: newCookie.name, state: "créé", color: "green"})
+  }
+
+  document.cookie = `${encodeURIComponent(newCookie.name)}=${encodeURIComponent(newCookie.value)};expires=${newCookie.expires.toUTCString()}`
+
+}
+
+function doesCookieExist(name) {
+
+  const cookies = document.cookie.replace(/\s/g, "").split(";");
+  const onlyCookiesName = cookies.map(cookie => cookie.split("=")[0])
+
+  const cookiePresence = onlyCookiesName.find(cookie => cookie === encodeURIComponent(name))
+
+  return cookiePresence;
+}
+
+const toastsContainer = document.querySelector(".toasts__container")
+
+function createToast({name, state, color}){
+  const toastInfo = document.createElement("p");
+  toastInfo.className = "toast";
+
+  toastInfo.textContent = `Cookie ${name} ${state}.`;
+  toastInfo.style.backgroundColor = color;
+  toastsContainer.appendChild(toastInfo)
+
+  setTimeout(() =>{
+    toastInfo.remove()
+  }, 2500)
+}
+
